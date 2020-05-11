7eb60bfff1a18a8a2f4ff864922c5c157eeecaf0
Kevin Jetten <jetten.kevin@gmail.com>
2020-05-11 22:14:51 +0200

    initial commit

---
+++ app/src/main/java/com/example/capchat_sensor_information_collector/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.capchat_sensor_information_collector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
